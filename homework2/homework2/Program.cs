bd8e43d68f5ad521837f283e1620ba7e2b7c65f4
İlayda <seninepostan@orneksite.com>
2025-10-14 21:22:38 +0300

    1.haftanın ödev dosyaları eklendi.

---
+++ homework2/homework2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dizideki elemanların toplamı
+
+            int[] sayilar = { 5, 8, 12, 15, 25, 39, 46, 50, 67, 89, 93 };
+            int toplam = 0;
+
+            foreach (int sayi in sayilar)
+            {
+                toplam += sayi;
+
+            }
+
+            Console.WriteLine("Sayıların toplamı:" + toplam);
+            Console.ReadLine();
+
+            //notasyonu O(n)'dir. çünkü dizide her eleman 1 defa gelir.
+        }
+    }
+}
---
+++ homework3/homework3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //matris çarpımı
+
+            int[,] matris1 = new int[2, 3] { { 2, 4, 9 }, { 3, 4, 5 } };
+            int[,] matris2 = new int[3, 2] { { 3, 7 }, {4, 8 }, { 5, 9 } };
+
+            int matris1Satir = matris1.GetLength(0); //m=2
+            int matris1Sutun = matris1.GetLength(1); //k=3
+            int matris2Satir = matris2.GetLength(0); //k=3
+            int matris2Sutun = matris2.GetLength(1); //n=2
+
+            int[,] sonuc = new int[matris1Satir, matris2Sutun];
+
+            // notasyon O(n3)'tür.
+
+            for (int i = 0; i < matris1Satir; i++)    // i: sonuç satırları (m). O(n)
+            {
+                for (int j = 0; j < matris2Sutun; j++)    // j: sonuç sütunları (n). O(n)
+                {
+                    int toplam = 0;
+                    for (int k = 0; k < matris1Sutun; k++)   // k: ortak boyut- mamtris1sütun ile matris2satir da aynı olmalı. O(n)
+                    {
+                        toplam += matris1[i, k] * matris2[k, j];
+                    }
+
+                    sonuc[i, j] = toplam;   // Sonucu C matrisine yaz
+                }
+            }
+            //Sonucu ekrana yazdır
+            Console.WriteLine("Sonuç Matrisi: ");
+            for (int i = 0; i < matris1Satir; i++)
+            {
+                for (int j = 0; j < matris2Sutun; j++)
+                {
+                    Console.Write(sonuc[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
